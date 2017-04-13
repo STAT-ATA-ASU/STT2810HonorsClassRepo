3307229d230bda2aa55b9dfbb6ce5a0af6cd30d1
Alan Arnholt <arnholtat@appstate.edu>
2017-04-13 09:12:09 -0400

    tweaks

--- Rscripts/regression.R
+++ Rscripts/regression.R
@@ -28,3 +28,27 @@ resid(mod1)
 library(plotly)
 g2 <- ggplotly(g1)
 g2
+# detach plotly
+detach("package:plotly", unload=TRUE)
+library(MASS)
+stepAIC(lm(mpg ~ ., data = mtcars), method = "backwards")
+mod2 <- lm(mpg ~ wt + qsec + am, data = mtcars)
+summary(mod2)
+plot(mod2, which = 1)
+##
+library(car)
+residualPlots(mod2)
+mod3 <- update(mod2, .~. + I(wt^2))
+summary(mod3)
+residualPlots(mod3)
+##
+mod4 <- update(mod3, .~. - am)
+summary(mod4)
+residualPlots(mod4)
+#
+mod.glm <- glm(mpg ~ poly(wt, 2) + qsec, data = mtcars)
+summary(mod.glm)
+# 
+library(boot)
+cv.glm(data = mtcars, glmfit = mod.glm, K = 5)$delta[1]
+# Note how this is considerably larger than rse from mod3!
